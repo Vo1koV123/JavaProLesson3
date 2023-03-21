e4edbc9aad5aa30b680e409022b5cb658315cabd
bodya <bodyanka229@gmail.com>
2023-03-21 15:35:15 +0200

    completed the task in main class

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,63 @@
-import package1.Circle;
-import package1.GeometricFigure;
-import package1.Square;
-import package1.Triangle;
+import package2.Cat;
+import package2.Human;
+import package2.Participant;
+import package2.Robot;
+import package2.Obstacle;
+import package2.Racetrack;
+import package2.Wall;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
 
 
-        GeometricFigure[] figure = new GeometricFigure[3];
-        figure[0] = new Circle(5);
-        figure[1] = new Square(4);
-        figure[2] = new Triangle(3,2);
+        Participant human = new Human(100, 1);
+        Participant robot = new Robot(200, 2);
+        Participant cat = new Cat(500, 4);
 
 
-        double totalArea = 0;
+        Obstacle wall = new Wall(3);
+        Obstacle racetrack = new Racetrack(150);
 
-        for (GeometricFigure figure1 : figure) {
-                totalArea += figure1.getArea();
+
+        Participant[] participants = {human, robot, cat};
+        Obstacle[] obstacles = {racetrack, wall};
+
+
+        test(participants, obstacles);
+
+
+    }
+
+    public static void test(Participant[] participants, Obstacle[] obstacles) {
+        for (Participant participant : participants) {
+            for (Obstacle obstacle : obstacles) {
+                if (obstacle.toString().contains("Racetrack")) {
+                    if (participant.getRunDistance() >= obstacle.overcome()) {
+                        participant.run();
+                        System.out.println("passed the racetrack");
+                    } else {
+                        participant.run();
+                        System.out.println("did not pass the racetrack");
+                        System.out.println("The passed distance: " + participant.getRunDistance() + " out of " + obstacle.overcome());
+                        break;
+                    }
+                } else {
+                    if (participant.getJumpDistance() >= obstacle.overcome()) {
+                        participant.jump();
+                        System.out.println("passed the wall");
+                    } else {
+                        participant.jump();
+                        System.out.println("did not pass the wall");
+                        System.out.println("The passed height: " + participant.getJumpDistance() + " out of " + obstacle.overcome());
+                        break;
+                    }
+                    System.out.println();
+                }
+            }
         }
-
-        System.out.println("The total area of all figures is " + totalArea);
-
     }
 }
