3c3e92826d5281b1fc9df56f2c2be226775b6a2e
bodya <bodyanka229@gmail.com>
2023-03-21 15:31:15 +0200

    added class Cat

---
+++ src/package2/Cat.java
@@ -0,0 +1,19 @@
+package package2;
+
+public class Cat extends Participant {
+
+
+    public Cat(int runDistance, int jumpDistance) {
+        super(runDistance, jumpDistance);
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("Cat jumping");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Cat running");
+    }
+}
