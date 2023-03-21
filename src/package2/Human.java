c41233105e7496727797ac06ee215edb7f2f87f5
bodya <bodyanka229@gmail.com>
2023-03-21 15:32:32 +0200

    added class Human

---
+++ src/package2/Human.java
@@ -0,0 +1,18 @@
+package package2;
+
+public class Human extends Participant {
+
+    public Human(int runDistance, int jumpDistance) {
+        super(runDistance, jumpDistance);
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("Human jumping");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Human running");
+    }
+}
