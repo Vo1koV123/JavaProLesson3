e00334d919ff3fef06253d45fbcec1e131e3975d
bodya <bodyanka229@gmail.com>
2023-03-21 15:34:30 +0200

    added class Wall

---
+++ src/package2/Wall.java
@@ -0,0 +1,19 @@
+package package2;
+
+public class Wall extends Obstacle {
+
+    private int height;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public int overcome() {
+        return height;
+    }
+}
