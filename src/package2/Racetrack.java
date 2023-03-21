a6752c238c529bdadbc55bafaa20ec5904b76536
bodya <bodyanka229@gmail.com>
2023-03-21 15:33:59 +0200

    added class Racetrack

---
+++ src/package2/Racetrack.java
@@ -0,0 +1,20 @@
+package package2;
+
+
+public class Racetrack extends Obstacle{
+
+    private int length;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Racetrack(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public int overcome() {
+        return length;
+    }
+}
